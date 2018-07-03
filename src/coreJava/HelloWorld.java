d2146710ab4c9c5641409f7457c16c565c0bd4e6
Senthil M <Senthil M@MININT-IEG6650.fios-router.home>
2018-07-03 17:43:43 -0400

    just commit

--- src/coreJava/HelloWorld.java
+++ src/coreJava/HelloWorld.java
@@ -6,6 +6,16 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World !!! ");
+		
+		int myIntValue = 5/3;
+		Float myFloatValue = 5f/3f;
+		Double myDoubleValue = 5d/3f;
+		
+		System.out.println(myIntValue);
+		System.out.println(myFloatValue);
+		System.out.println(myDoubleValue);
+		
+		
 	}
 
 }
